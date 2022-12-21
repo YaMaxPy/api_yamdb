c546731102511a1f8e5f730ea784eab0e69084a2
Игорь Арефьев <djifrost@mail.ru>
2022-12-21 22:25:56 +0300

    v1.0

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,43 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+
+from reviews.models import Comment, Review, Title
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    def validate(self, value):
+        author = self.context['request'].user
+        title_id = (self.context['request'].
+                    parser_context['kwargs'].get('title_id'))
+        title = get_object_or_404(
+            Title,
+            id=title_id
+        )
+        if (self.context['request'].method == 'POST'
+                and title.reviews.filter(author=author).exists()):
+            raise serializers.ValidationError(
+                f'Отзыв на произведение {title.name} уже существует'
+            )
+        return value
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    review = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Comment

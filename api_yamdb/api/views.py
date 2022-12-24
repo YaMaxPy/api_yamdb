0e5ab536159bc2be2f69b9a125122ba6d1cb091b
Максим Радченко <tagr2101@yandex.ru>
2022-12-24 19:50:54 +0300

    11 failed, 66 passed

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -111,7 +111,7 @@ class TitleViewSet(viewsets.ModelViewSet):
         Avg("reviews__score")
     ).order_by("name")
     serializer_class = TitleSerializer
-    permission_classes = (IsAdmin,)
+    permission_classes = (IsAdminUserOrReadOnly,)
 
     def get_serializer_class(self):
         if self.action in ("retrieve", "list"):
@@ -125,7 +125,7 @@ class CategoryViewSet(mixins.ListModelMixin,
                       viewsets.GenericViewSet,):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (IsAdmin,)
+    permission_classes = (IsAdminUserOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name",)
     lookup_field = "slug"
@@ -134,7 +134,7 @@ class CategoryViewSet(mixins.ListModelMixin,
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = (IsAdmin,)
+    permission_classes = (IsAdminUserOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name",)
     lookup_field = "slug"
